77223769b307973e337923881366b90673f7edcd
WALLACE INACIO GOMES DE SOUSA <wallace.inacio@cemig.com.br>
2024-07-26 09:20:18 -0300

    add DevJourney

--- Python/my_torrent/client.py
+++
@@ -1,9 +0,0 @@
-from torrent import Torrent
-from download import Download
-
-if __name__ == '__main__':
-    arquivo_torrent = input("Digite o caminho para o arquivo .torrent: ")
-    torrent = Torrent(arquivo_torrent)
-
-    download = Download(torrent)
-    download.iniciar()
