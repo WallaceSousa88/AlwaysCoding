d40e4262ce025190751542f81a520d77d642f4ad
WALLACE INACIO GOMES DE SOUSA <wallace.inacio@cemig.com.br>
2024-09-06 08:56:57 -0300

    add PMV-SI-2024

---
+++ C#/PMV-SI-2024/GestaoMedicamentos.Produto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestaoMedicamentos.Produto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ C#/PMV-SI-2024/GestaoMedicamentos.Relatorios/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestaoMedicamentos.Relatorios.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
