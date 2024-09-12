65e0d515972bffcfa623920478b416724dfd085f
WALLACE INACIO GOMES DE SOUSA <wallace.inacio@cemig.com.br>
2024-09-12 09:10:59 -0300

    upd PMV-SI-2024

---
+++ C#/PMV-SI-2024/GestaoMedicamentos.Produto/Data/MedicamentosContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GestaoMedicamentos.Produto.Data
+{
+    public class MedicamentosContext : DbContext
+    {
+        public MedicamentosContext(DbContextOptions<MedicamentosContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Lote> Lotes { get; set; }
+        public DbSet<Transacao> Transacoes { get; set; }
+    }
+}
