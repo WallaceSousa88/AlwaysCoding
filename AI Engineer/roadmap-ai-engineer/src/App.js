a03fd2a6eeb3e435fcdeb86020ba99da9f7e4ddf
WallaceSousa88 <wallaceinaciogs@gmail.com>
2025-02-07 19:55:49 -0300

    upd AI Engineer

--- AI Engineer/roadmap-ai-engineer/src/App.js
+++ AI Engineer/roadmap-ai-engineer/src/App.js
@@ -7,6 +7,20 @@ function App() {
       <header className="App-header">
         <div className="App-nav">
           <img src={logo} className="App-logo" alt="logo" />
+          <nav>
+            <ul>
+              <li><a href="#introduction">Introdução</a></li>
+              <li><a href="#pre-trained-models">Using Pre-trained Models</a></li>
+              <li><a href="#openai-api">OpenAI API</a></li>
+              <li><a href="#ai-safety-ethics">AI Safety and Ethics</a></li>
+              <li><a href="#opensource-ai">OpenSource AI</a></li>
+              <li><a href="#embeddings-vector-databases">Embeddings & Vector Databases</a></li>
+              <li><a href="#rag-implementation">RAG & Implementation</a></li>
+              <li><a href="#ai-agents">AI Agents</a></li>
+              <li><a href="#multimodal-ai">Multimodal AI</a></li>
+              <li><a href="#development-tools">Development Tools</a></li>
+            </ul>
+          </nav>
         </div>
         <h1>Importância da Engenharia de IA no Desenvolvimento de Produtos</h1>
         <p>
@@ -19,7 +33,7 @@ function App() {
       <footer className="App-footer">
         <p>
           Este website faz parte de um projeto onde estou seguindo a trilha de aprendizado do <a href="https://roadmap.sh/ai-engineer" target="_blank" rel="noopener noreferrer">roadmap de AI Engineer</a>.<br></br> A cada etapa desenvolvida, vou salvar o conteúdo aqui, permitindo acompanhamento e compartilhamento do aprendizado.
-        </p>        
+        </p>
       </footer>
     </div>
   );
